eaf26a80eb81c3719a9708c267c744db222a715d
Shardendu Mishra <shardendumishra01@gmail.com>
2024-07-20 14:23:05 +0530

    LeetCode Problems

---
+++ __Difficult To Understand For Later Understanding/29ValidMatrixGivenRowAndColumn.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC target   ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx")
+#pragma GCC optimize ("Ofast")
+#pragma GCC optimize ("-ffloat-store")
+#pragma GCC optimize ("Ofast")
+
+static auto _ = [] () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+#define frw(i, len) for(ll i = 0   ; i < len ; i++)
+#define frb(i, len) for(ll i = len ; i >= 0  ; i--)
+
+#define YES cout<<"Yes \n";
+#define NOO cout<<"No  \n";
+#define nl cout<<"\n";
+
+#define MAX_SIZE 10000
+#define nptr nullptr
+
+typedef stringstream strgm;
+typedef long long int ll;
+typedef const int c_int;
+typedef unsigned unsg;
+typedef double dbl;
+
+typedef vector<vector<string>> vvstr;
+typedef vector<vector<bool>> vvbl;
+typedef vector<vector<int>> vvint;
+typedef vector<vector<ll>> vvll;
+
+typedef vector<string> vstr;
+typedef vector<int> vint;
+typedef vector<bool> vbl;
+typedef vector<ll> vll;
+
+typedef stack<string> sstr;
+typedef stack<bool> sbl;
+typedef stack<int> sint;
+typedef stack<ll> sll;
+
+typedef queue<string> qstr;
+typedef queue<bool> qbl;
+typedef queue<int> qint;
+typedef queue<ll> qll;
+
+c_int MOD=1e9 + 7;
+c_int Mx_row=100;
+c_int Mx_col=100;
+int size_stack=0;
+int InvrsnCnt=0;
+int size_arr=0;
+int size_ll=0;
+int top=-1;
+
+
+class Solution {
+public:
+    vector<vector<int>> restoreMatrix(vector<int>& rowSum,
+                                      vector<int>& colSum) {
+        int N = rowSum.size();
+        int M = colSum.size();
+
+        vector<vector<int>> origMatrix(N, vector<int>(M, 0));
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                origMatrix[i][j] = min(rowSum[i], colSum[j]);
+
+                rowSum[i] -= origMatrix[i][j];
+                colSum[j] -= origMatrix[i][j];
+            }
+        }
+
+        return origMatrix;
+    }
+};
