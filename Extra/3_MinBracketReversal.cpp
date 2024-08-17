f47c0377cb18db0f7a18e74284d3d4c676cec687
Shardendu Mishra <shardendumishrabrave@gmail.com>
2024-08-17 16:21:38 +0530

    LeetCode Problem

--- Extra/3_MinBracketReversal.cpp
+++ Extra/3_MinBracketReversal.cpp
@@ -9,7 +9,40 @@ static auto _ = [] () {
     return 0;
 }();
 
+int solve(string& s){
+    stack<char> st;
+    for(int i=0;i<s.size();i++){
+        if(s[i] == '('){
+            st.push(s[i]);
+        }else{
+            if(s[i] == ')' && st.top() == '('){
+                st.pop();
+            }else{
+                st.push(s[i]);
+            }
+        }
+    }
+    int a = 0;
+    int b = 0;
+    while(!st.empty()){
+        if(st.top() == '('){
+            a += 1;
+        }else{
+            b += 1;
+        }
+        st.pop();
+    }
+    return ((a+1)/2 + (b+1)/2);
+}
+
 int main(){
-    
+    string s1 = "}{}{";
+    string s2 = "}}}}";
+    string s3 = "{{{{";
+
+    cout<<solve(s1)<<endl;
+    cout<<solve(s2)<<endl;
+    cout<<solve(s3)<<endl;
+
     return 0;
 }
