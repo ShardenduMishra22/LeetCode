ba545e2aa7663e7264b9ad339bcd998ec28005c6
Shardendu Mishra <shardendumishrabrave@gmail.com>
2024-08-18 21:45:16 +0530

    LeetCode Problems

--- Extra/leetcode.cpp
+++ Extra/leetcode.cpp
@@ -1,38 +1,34 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
 public:
-    bool IsPal(string& s,int i,int j,vector<vector<int>>& dp){
-        if(s.size() <= 1){
+    bool IsPal(string& s, int i, int j, vector<vector<int>>& dp) {
+        if (i >= j) 
             return true;
-        }
-        if(i >= j){
-            return true;
-        }
-        if(dp[i][j] != false){
+        if (dp[i][j] != -1)
             return dp[i][j];
-        }
-        if(s[i] != s[j]){
-            return false;
-        }
-        return dp[i][j] = IsPal(s,i+1,j-1,dp);
+        if (s[i] != s[j]) 
+            return dp[i][j] = false;
+        
+        return dp[i][j] = IsPal(s, i + 1, j - 1, dp);
     }
 
     string largestPalindrome(int n, int k) {
         int num = 0;
-        for(int i=0;i<n;i++){
-            num = num*10 + 9;
+        for (int i = 0; i < n; i++) {
+            num = num * 10 + 9;
         }
 
-        for(int i = num;i >= 0;i--){
-            string s = to_string(i);
-            vector<vector<bool>> dp(s.size().vector<bool>(s.size(),false));
-            if(i % k == 0 && IsPal(s,0,s.size()-1,dp)){
-                return s;
+        for (int i = num; i >= 0; i--) {
+            if (i % k == 0) {
+                string s = to_string(i);
+                vector<vector<int>> dp(s.size(), vector<int>(s.size(), -1));
+                if (IsPal(s, 0, s.size() - 1, dp)) {
+                    return s;
+                }
             }
         }
         return "";
-
     }
 };
