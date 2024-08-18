e2a13cd11d8ff2ac896b02a32fedb074ad05dbce
Shardendu Mishra <shardendumishrabrave@gmail.com>
2024-08-18 21:58:30 +0530

    LeetCode Problem

--- Day 70/2_CountPrimes.cpp
+++ Day 70/2_CountPrimes.cpp
@@ -1,3 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Solution {
+public:
+    int countPrimes(int n) {
+        int cnt = 0;
+        vector<bool> prm(n + 1, true);
+        prm[0] = prm[1] = false;
+        for (int i = 2; i < n; i++) {
+            if (prm[i]) {
+                cnt++;
+                for (int j = i * 2; j < n; j = j + i) {
+                    prm[j] = 0;
+                }
+            }
+        }
+        return cnt;
+    }
+};
