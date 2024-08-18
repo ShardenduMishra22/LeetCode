2b4c8ec5c44e727b6ef30505372d26756c7b4b2b
Shardendu Mishra <shardendumishrabrave@gmail.com>
2024-08-18 21:59:15 +0530

    LeetCode Problem

--- Day 70/3_PrimePairsWithtarget.cpp
+++ Day 70/3_PrimePairsWithtarget.cpp
@@ -1,3 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Solution {
+public:
+    void solve(vector<bool>& prm,int& n){
+        prm[0] = prm[1] = false;
+        for(int i=2;i<n;i++){
+            if(prm[i]){
+                for(int j=i*2;j<n;j+=i){
+                    prm[j] = false;
+                }
+            }
+        }
+    } 
+
+    vector<vector<int>> findPrimePairs(int n) {
+        vector<bool> prm(n,true);
+        solve(prm,n);
+        vector<vector<int>> ans;
+        for(int i=2;i<=n/2;i++){
+            if(prm[i] && prm[n-i]){
+                ans.push_back({i,n-i});
+            }
+        }
+        return ans;
+    }
+};
