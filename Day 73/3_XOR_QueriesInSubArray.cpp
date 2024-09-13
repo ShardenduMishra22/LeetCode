4155981ddca6df2cbeb001aa7b5a4f7408f3b57a
Shardendu Mishra <shardendumishra01@gmail.com>
2024-09-13 09:50:07 +0530

    LeetCode Problems

---
+++ Day 73/3_XOR_QueriesInSubArray.cpp
@@ -0,0 +1,41 @@
+// class Solution {
+// public:
+//     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+//         int n = arr.size();
+
+//         vector<int> PrfXOR(n,0);
+//         PrfXOR[0] = arr[0];
+
+//         for(int i=1;i<arr.size();i++){
+//             PrfXOR[i] = PrfXOR[i-1] ^ arr[i]; 
+//         }
+
+//         vector<int> res;
+//         for(auto& i : queries){
+//             int l = i[0];
+//             int r = i[1];
+
+//             int xor_val = PrfXOR[r] ^ (l == 0 ? 0 : PrfXOR[l-1]);
+//             res.push_back(xor_val);
+//         }
+
+//         return res;
+//     }
+// };
+
+// class Solution {
+// public:
+//     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+//         vector<int> res;
+
+//         for (auto& q : queries) {
+//             int xor_val = 0;
+//             for (int i = q[0]; i <= q[1]; i++) {
+//                 xor_val ^= arr[i];
+//             }
+//             res.push_back(xor_val);
+//         }
+
+//         return res;
+//     }
+// };
